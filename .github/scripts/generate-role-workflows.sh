f8c8842103cad185d808f051d66a5535da8fe3cc
Christian Berendt <berendt@osism.tech>
2021-07-02 12:46:32 +0200

    github: fix molecule jobs
    Signed-off-by: Christian Berendt <berendt@osism.tech>

---
+++ .github/scripts/generate-role-workflows.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# usage: bash scripts/generate-job-workflows.sh
+
+# requirements: pip3 install jinja-cli
+
+for role in cleanup configuration docker_compose facts hostname kompose kubectl microcode motd operator packages podman proxy repository resolvconf services sshconfig state sysctl systohc trivy; do
+  jinja -D ansible_role $role \
+        -D ansible_versions "['4.2.0']" \
+        -D python_versions "['3.8']" \
+        -D docker_images "['ubuntu:20.04']" \
+    templates/test-role.yml.j2 > workflows/test-role-$role.yml
+done
