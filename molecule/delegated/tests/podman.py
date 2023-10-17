a942f4ab912fcc33e5f36671be08969d4276b67a
Gondermann <gondermann@b1-systems.de>
2023-10-17 16:19:57 +0200

    Add podman test
    Signed-off-by: Gondermann <gondermann@b1-systems.de>

--- molecule/delegated/tests/podman.py
+++ molecule/delegated/tests/podman.py
@@ -1,8 +1,14 @@
-import os
-import pytest
 from .util.util import get_ansible, get_variable
 
 testinfra_runner, testinfra_hosts = get_ansible()
 
-def test_default():
-    assert False
+def test_pkg(host):
+    package_name = get_variable(host, "podman_package_name")
+    assert package_name != ""
+
+    package = host.package(package_name)
+    assert package.is_installed
+
+    package_name = "uidmap"
+    package = host.package(package_name)
+    assert package.is_installed
