215727fd52457e3f3e596fe1796a159126d565fc
Sebastian Neumann <neumann@b1-systems.de>
2024-01-18 16:08:36 +0100

    Add autodetection of CPU architecture for kompose role
    Signed-off-by: Sebastian Neumann <neumann@b1-systems.de>

--- molecule/delegated/tests/kompose.py
+++ molecule/delegated/tests/kompose.py
@@ -28,8 +28,24 @@ def test_kompose_binary_exists(host):
 
 
 def test_kompose_binary_checksum(host):
-    kompose_checksum = get_variable(host, "kompose_checksum")
+    ansible_arch = get_variable(host, "ansible_architecture", True)
+    kompose_checksums = get_variable(host, "kompose_checksums")
     kompose_path = "/usr/local/bin/kompose"
-    f = host.file(kompose_path)
-    assert f.exists
-    assert f.sha256sum == kompose_checksum
+
+    if ansible_arch == "x86_64":
+        kompose_checksum = kompose_checksums.get("amd64")
+        f = host.file(kompose_path)
+        assert f.exists
+        assert f.sha256sum == kompose_checksum
+
+    if ansible_arch == "aarch64" or ansible_arch == "arm64":
+        kompose_checksum = kompose_checksums.get("arm64")
+        f = host.file(kompose_path)
+        assert f.exists
+        assert f.sha256sum == kompose_checksum
+
+    if ansible_arch == "armv6l" or ansible_arch == "armv7l":
+        kompose_checksum = kompose_checksums.get("arm")
+        f = host.file(kompose_path)
+        assert f.exists
+        assert f.sha256sum == kompose_checksum
