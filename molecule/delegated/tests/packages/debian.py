135f8d6c0d200dd705965333a89b2c8228d637b9
Sebastian Neumann <neumann@b1-systems.de>
2024-01-29 11:34:08 +0100

    Use packages_ as prefix of variables in packages role
    Signed-off-by: Sebastian Neumann <neumann@b1-systems.de>

--- molecule/delegated/tests/packages/debian.py
+++ molecule/delegated/tests/packages/debian.py
@@ -13,10 +13,10 @@ def check_ansible_os_family(host):
 def test_package_upgrade(host):
     check_ansible_os_family(host)
 
-    upgrade_packages = get_variable(host, "upgrade_packages")
+    upgrade_packages = get_variable(host, "packages_upgrade")
 
     if not upgrade_packages:
-        pytest.skip("upgrade_packages is not True")
+        pytest.skip("packages_upgrade is not True")
 
     upgradable = host.check_output("apt list --upgradable 2>/dev/null | wc -l")
     # subtract 1 to account for the header line
