54d21b5fe1d5294c802ee022a4a4af43d4ac8723
Sebastian Neumann <neumann@b1-systems.de>
2024-02-21 15:46:47 +0100

    RHEL implementation for podman
    Signed-off-by: Sebastian Neumann <neumann@b1-systems.de>

--- molecule/delegated/tests/podman/debian.py
+++ molecule/delegated/tests/podman/debian.py
@@ -1,9 +1,18 @@
-from .util.util import get_ansible, get_variable
+import pytest
+
+from ..util.util import get_ansible, get_variable
 
 testinfra_runner, testinfra_hosts = get_ansible()
 
 
+def check_ansible_os_family(host):
+    if get_variable(host, "ansible_os_family", True) != "Debian":
+        pytest.skip("ansible_os_family mismatch")
+
+
 def test_pkg(host):
+    check_ansible_os_family(host)
+
     package_name = get_variable(host, "podman_package_name")
     assert package_name != ""
 
---
+++ molecule/delegated/tests/podman/redhat.py
@@ -0,0 +1,20 @@
+import pytest
+
+from ..util.util import get_ansible, get_variable
+
+testinfra_runner, testinfra_hosts = get_ansible()
+
+
+def check_ansible_os_family(host):
+    if get_variable(host, "ansible_os_family", True) != "RedHat":
+        pytest.skip("ansible_os_family mismatch")
+
+
+def test_pkg(host):
+    check_ansible_os_family(host)
+
+    package_name = get_variable(host, "podman_package_name")
+    assert package_name != ""
+
+    package = host.package(package_name)
+    assert package.is_installed
