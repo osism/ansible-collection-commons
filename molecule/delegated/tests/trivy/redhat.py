450947ffa7aecdb3ccb679b7a54d3edcdd041cc9
Sebastian Neumann <neumann@b1-systems.de>
2024-02-26 15:03:38 +0100

    RHEL implementation for trivy
    Signed-off-by: Sebastian Neumann <neumann@b1-systems.de>

---
+++ molecule/delegated/tests/trivy/redhat.py
@@ -0,0 +1,74 @@
+import pytest
+
+from ..util.util import (
+    get_ansible,
+    get_variable,
+    get_from_url,
+    extract_url_from_variable,
+    get_centos_repo_key,
+)
+
+testinfra_runner, testinfra_hosts = get_ansible()
+
+
+def check_ansible_os_family(host):
+    if get_variable(host, "ansible_os_family", True) != "RedHat":
+        pytest.skip("ansible_os_family mismatch")
+
+
+def test_package(host):
+    """Check if the packages are installed."""
+    check_ansible_os_family(host)
+
+    if get_variable(host, "trivy_configure_repository"):
+        pkg_name = "yum-utils"
+        pkg = host.package(pkg_name)
+        assert pkg.is_installed
+
+    pkg_name = get_variable(host, "trivy_package_name")
+    pkg = host.package(pkg_name)
+    assert pkg.is_installed
+
+
+def test_trivy_gpg_key_present(host):
+    """Check if the GPG key for the trivy repository is correctly added."""
+    check_ansible_os_family(host)
+
+    trivy_configure_repository = get_variable(host, "trivy_configure_repository")
+
+    if not trivy_configure_repository:
+        pytest.skip("trivy_configure_repository not configured")
+
+    all_keys = host.run(
+        "rpm -qa gpg-pubkey* --qf '%{NAME}-%{VERSION}-%{RELEASE} --> %{SUMMARY}\n%{DESCRIPTION}\n\n'"
+    ).stdout
+    installed_key = get_centos_repo_key(host, "trivy public key")
+    assert installed_key in all_keys
+
+    trivy_repository_key_url = get_variable(host, "trivy_redhat_repository_key")
+    key_content = get_from_url(trivy_repository_key_url)
+    assert installed_key in key_content
+
+
+def test_trivy_repository_configured(host):
+    """Check if the Trivy repository is correctly configured."""
+    check_ansible_os_family(host)
+
+    trivy_configure_repository = get_variable(host, "trivy_configure_repository")
+
+    if not trivy_configure_repository:
+        pytest.skip("trivy_configure_repository not configured")
+
+    extracted_url = extract_url_from_variable(host, "trivy_redhat_repository")
+
+    repo_file = host.file("/etc/yum.repos.d/trivy.repo")
+    assert repo_file.exists
+    assert repo_file.user == "root"
+    assert repo_file.group == "root"
+    assert repo_file.mode == 0o600
+
+    with host.sudo("root"):
+        repo_file_content = host.check_output("cat /etc/yum.repos.d/trivy.repo")
+        assert (
+            extracted_url in repo_file_content
+        ), "The extracted URL is not present in the configuration file"
