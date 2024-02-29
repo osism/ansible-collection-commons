be25844c4ef9246c1606f61ba692fc12df323873
Sebastian Neumann <neumann@b1-systems.de>
2024-02-29 14:57:14 +0100

    RHEL implementation for microcode role
    Signed-off-by: Sebastian Neumann <neumann@b1-systems.de>

--- molecule/delegated/tests/microcode.py
+++ molecule/delegated/tests/microcode.py
@@ -4,17 +4,33 @@ testinfra_runner, testinfra_hosts = get_ansible()
 
 
 def test_microcode_packages(host):
-    microcode_packages_default = get_variable(host, "microcode_packages_default")
     microcode_packages_extra = get_variable(host, "microcode_packages_extra")
-    microcode_packages = get_variable(host, "microcode_packages")
 
-    microcode_packages = jinja_list_concat(
-        microcode_packages, [microcode_packages_default, microcode_packages_extra]
-    )
+    if get_variable(host, "ansible_os_family", True) == "Debian":
+        microcode_packages_default = get_variable(host, "microcode_packages_default")
+        microcode_packages_debian = get_variable(host, "microcode_packages_debian")
+        microcode_packages = jinja_list_concat(
+            microcode_packages_debian,
+            [microcode_packages_default, microcode_packages_extra],
+        )
+        assert type(microcode_packages) is list
+        assert len(microcode_packages) > 0
 
-    assert type(microcode_packages) is list
-    assert len(microcode_packages) > 0
+        for package_name in microcode_packages:
+            package = host.package(package_name)
+            assert package.is_installed
 
-    for package_name in microcode_packages:
-        package = host.package(package_name)
-        assert package.is_installed
+    if get_variable(host, "ansible_os_family", True) == "RedHat":
+        microcode_packages_default = get_variable(host, "microcode_packages_centos")
+        microcode_packages_redhat = get_variable(host, "microcode_packages_redhat")
+
+        microcode_packages = jinja_list_concat(
+            microcode_packages_redhat,
+            [microcode_packages_default, microcode_packages_extra],
+        )
+        assert type(microcode_packages) is list
+        assert len(microcode_packages) > 0
+
+        for package_name in microcode_packages:
+            package = host.package(package_name)
+            assert package.is_installed
