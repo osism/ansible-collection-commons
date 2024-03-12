44b97438e4a6fcb656c9dc53d3f84de6baa7d97b
Gondermann <gondermann@b1-systems.de>
2024-03-12 12:24:01 +0100

    Add molecule test for role user
    Signed-off-by: Gondermann <gondermann@b1-systems.de>

--- molecule/delegated/tests/user.py
+++
@@ -1,7 +0,0 @@
-from .util.util import get_ansible
-
-testinfra_runner, testinfra_hosts = get_ansible()
-
-
-def test():
-    assert False, "Unittest not implemented"
---
+++ molecule/delegated/tests/user/accounts.py
@@ -0,0 +1,83 @@
+import pytest
+
+from ..util.util import get_ansible, get_variable
+
+testinfra_runner, testinfra_hosts = get_ansible()
+
+
+def check_user_manage_type(host):
+    if get_variable(host, "user_manage_type") != "accounts":
+        pytest.skip("user_manage_type mismatch")
+
+
+def test_accounts_present(host):
+    check_user_manage_type(host)
+
+    user_list = get_variable(host, "user_list")
+    assert type(user_list) is list
+
+    if len(user_list) == 0:
+        pytest.skip("user_list is empty")
+
+    for user_entry in user_list:
+        user_name = user_entry["name"]
+
+        assert host.group(user_name).exists
+
+        user = host.user(user_name)
+        assert user.exists
+        assert user.shell == "/usr/bin/bash"
+        assert user.group == user_name
+
+        hd = host.file(f"/home/{user_name}")
+        assert hd.exists
+        assert hd.is_directory
+
+        with host.sudo(user_name):
+            keyfile = host.file(f"/home/{user_name}/.ssh/authorized_keys")
+            assert keyfile.exists
+            assert not keyfile.is_directory
+            assert user_entry["key"] in keyfile.content_string
+
+
+def test_sudo(host):
+    check_user_manage_type(host)
+
+    assert host.package("sudo").is_installed
+
+    user_list = get_variable(host, "user_list")
+    assert type(user_list) is list
+
+    if len(user_list) == 0:
+        pytest.skip("user_list is empty")
+
+    for user_entry in user_list:
+        user_name = user_entry["name"]
+
+        with host.sudo("root"):
+            f = host.file(f"/etc/sudoers.d/{user_name}")
+            assert f.exists
+            assert not f.is_directory
+            assert f.mode == 0o440
+            assert f.user == "root"
+            assert f.group == "root"
+            assert "NOPASSWD: ALL" in f.content_string
+
+
+def test_accounts_absent(host):
+    check_user_manage_type(host)
+
+    user_list = get_variable(host, "user_delete")
+    assert type(user_list) is list
+
+    if len(user_list) == 0:
+        pytest.skip("user_delete is empty")
+
+    for user_name in user_list:
+        assert not host.group(user_name).exists
+
+        user = host.user(user_name)
+        assert not user.exists
+
+        hd = host.file(f"/home/{user_name}")
+        assert not hd.exists
