680c30ff087c98605e83092a2797692a75f375a9
Eike Waldt <waldt@b1-systems.de>
2024-05-28 20:59:10 +0200

    kubectl: make required packages distro specific (#654)
    Signed-off-by: Eike Waldt <waldt@b1-systems.de>

--- molecule/delegated/tests/kubectl/debian.py
+++ molecule/delegated/tests/kubectl/debian.py
@@ -58,10 +58,3 @@ def test_kubectl_repository_configured(host):
         assert (
             extracted_url in repo_file_content
         ), "The extracted URL is not present in the kubectl configuration file"
-
-
-def test_kubectl_package_installed(host):
-    """Check if the kubectl package is installed."""
-    kubectl_package_name = get_variable(host, "kubectl_package_name")
-    kubectl_pkg = host.package(kubectl_package_name)
-    assert kubectl_pkg.is_installed
--- molecule/delegated/tests/kubectl/main.py
+++ molecule/delegated/tests/kubectl/main.py
@@ -1,13 +1,16 @@
 from ..util.util import (
     get_ansible,
-    get_variable,
+    get_family_role_variable,
 )
 
 testinfra_runner, testinfra_hosts = get_ansible()
 
 
-def test_kubectl_package_installed(host):
-    """Check if the kubectl package is installed."""
-    kubectl_package_name = get_variable(host, "kubectl_package_name")
-    kubectl_pkg = host.package(kubectl_package_name)
-    assert kubectl_pkg.is_installed
+def test_packages(host):
+
+    required_packages = get_family_role_variable(host, "kubectl_required_packages")
+    assert type(required_packages) is list
+
+    for package_name in required_packages:
+        package = host.package(package_name)
+        assert package.is_installed
