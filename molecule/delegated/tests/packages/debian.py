e91ef72e5a8a047ea5a34e95a75373092f098763
Christian Berendt <berendt@osism.tech>
2024-08-15 21:31:12 +0200

    packages: make needrestart mode configurable
    With the packages_needstart_mode it is possible to change the needstart
    mode. Possible values are a (auto), i (interactive) and l (list only).
    
    The default value is l (list only). The Ubuntu default is i
    (interactive). We prefer to only list services that should be restarted.
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- molecule/delegated/tests/packages/debian.py
+++ molecule/delegated/tests/packages/debian.py
@@ -10,7 +10,7 @@ def check_ansible_os_family(host):
         pytest.skip("ansible_os_family mismatch")
 
 
-def test_package_upgrade(host):
+def test_packages_upgrade(host):
     check_ansible_os_family(host)
 
     upgrade_packages = get_variable(host, "upgrade_packages")
@@ -22,3 +22,16 @@ def test_package_upgrade(host):
     # subtract 1 to account for the header line
     num_upgradable = int(upgradable.strip()) - 1
     assert num_upgradable == 0
+
+
+def test_packages_needrestart_mode(host):
+    check_ansible_os_family(host)
+
+    packages_needrestart_mode = get_variable(host, "packages_needrestart_mode")
+    needrestart_configuration = host.file(
+        "/etc/needrestart/needrestart.conf"
+    ).content_string
+    assert (
+        f"$nrconf{{restart}} = '{packages_needrestart_mode}';"
+        in needrestart_configuration
+    )
