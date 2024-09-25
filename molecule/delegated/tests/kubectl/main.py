a0917f79dddbbb64e46b9e56e3286c26040d0c8a
gndrmnn <120007213+gndrmnn@users.noreply.github.com>
2024-09-25 16:30:56 +0200

    kubectl: enhance CI tests (#722)
    Part of osism/issues#1132
    
    Signed-off-by: Gondermann <gondermann@b1-systems.de>

--- molecule/delegated/tests/kubectl/main.py
+++ molecule/delegated/tests/kubectl/main.py
@@ -7,10 +7,17 @@ testinfra_runner, testinfra_hosts = get_ansible()
 
 
 def test_packages(host):
-
     required_packages = get_family_role_variable(host, "kubectl_required_packages")
     assert type(required_packages) is list
 
     for package_name in required_packages:
         package = host.package(package_name)
         assert package.is_installed
+
+
+def test_cli(host):
+    result = host.run("kubectl version")
+    assert "client version" in result.stdout.lower()
+
+    # exit code may be != 0, when no cluster is found
+    # (it is usually not in these tests)
