fb877bab6abcdcdf1e55c2ab8a525ef80835d56a
Gondermann <gondermann@b1-systems.de>
2024-09-25 12:26:01 +0200

    k9s: Enhance CI tests
    Signed-off-by: Gondermann <gondermann@b1-systems.de>

--- molecule/delegated/tests/k9s.py
+++ molecule/delegated/tests/k9s.py
@@ -1,3 +1,14 @@
-def test_runc_package(host):
+from .util.util import get_ansible
+
+testinfra_runner, testinfra_hosts = get_ansible()
+
+
+def test_package(host):
     package = host.package("k9s")
     assert package.is_installed
+
+
+def test_cli(host):
+    result = host.run("k9s info")
+    assert result.rc == 0
+    assert "version" in result.stdout.lower()
